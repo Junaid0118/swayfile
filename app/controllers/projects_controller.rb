26ef8736fe61be35c78cb6044af6ac3430caf0f4
new user name <junaidiqbalshah011@gmail.com>
2023-09-22 19:14:45 +0100

    Worked on the project creation form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
+    before_action :project_params, only: :create
     def index
         render layout: "projects"
     end
@@ -17,5 +18,27 @@ class ProjectsController < ApplicationController
 
     def activity
         render layout: "projects"
+    end
+
+    def create
+        byebug
+        project = Project.new(project_params)
+        if project.save
+            render json: project, status: :created
+        else
+            render json: {}, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def project_params
+        {
+            name: params[:name],
+            project_type: params[:type],
+            description: params[:description],
+            status: params[:status],
+            date: params[:date]
+        }
     end
 end
