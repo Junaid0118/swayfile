29829cdc20d378de6a7f0c982ca02433ecf1dcf6
new user name <junaidiqbalshah011@gmail.com>
2023-09-27 18:31:07 +0100

    Implemented the cloudinary for active storage

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,8 @@
+# config/initializers/cloudinary.rb
+
+Cloudinary.config do |config|
+    config.cloud_name = "dcyq3cxsc"
+    config.api_key = "215952149461682"
+    config.api_secret = "Fqzd5TyOFUMnJgP2OgQ_ZPcdunk"
+end
+  
