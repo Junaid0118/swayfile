c12c057ce220c7673a849fb08bdfe851017d7999
new user name <junaidiqbalshah011@gmail.com>
2023-11-10 13:36:59 +0000

    Configured mailer preview for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,10 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.show_previews = true
+  config.action_mailer.preview_path = "#{Rails.root}/test/mailers/previews"
+
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
